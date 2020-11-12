01ad1704047f4f8022dafbb960509c989204561f
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2020-11-12 16:29:01 +0700

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -26,17 +26,14 @@ async def filter_incoming_handler(handler):
             if not filters:
                 return
             for trigger in filters:
-                print(str(trigger))
                 pattern = (
                     r"( |^|[^\w])" + escape(trigger.keyword) + r"( |$|[^\w])")
                 pro = search(pattern, name, flags=IGNORECASE)
-                print(str(pro))
                 if pro:
                     if trigger.f_mesg_id:
-                        print(str(trigger.f_mesg_id))
-                        msg_o = await handler.client.get_messages(entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id))
-                        await handler.reply(msg_o.message, file=msg_o.media)
-                    elif trigger.reply:
+                        #msg_o = await handler.client.get_messages(entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id))
+                        #await handler.reply(msg_o.message, file=msg_o.media)
+                    #elif trigger.reply:
                         await handler.reply(trigger.reply)
                 break
     except AttributeError:
