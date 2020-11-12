6135c2abee4796434ba7c3b611f60b06a8a8a311
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2020-11-12 17:15:27 +0700

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -31,10 +31,11 @@ async def filter_incoming_handler(handler):
                 pro = search(pattern, name, flags=IGNORECASE)
                 if pro:
                     if trigger.f_mesg_id:
-                        msg_o = handler.client.get_messages(entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id))
-                        handler.reply(msg_o.message, file=msg_o.media)
-                    #elif trigger.reply:
-                        #await handler.reply(trigger.reply)
+                        msg_o = await handler.client.get_messages(entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id))
+                        await handler.reply(msg_o.message, file=msg_o.media)
+                    elif trigger.reply:
+                        await handler.reply(trigger.reply)
+                    break
     except AttributeError:
         pass
 
