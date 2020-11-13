9a7015138f436053c97893243507e195aae5e445
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2020-11-13 07:22:53 +0700

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -31,7 +31,7 @@ async def filter_incoming_handler(handler):
                 pro = search(pattern, name, flags=IGNORECASE)
                 if pro:
                     if trigger.f_mesg_id:
-                        msg_o = await handler.client.get_messages(entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id))
+                        #msg_o = await handler.client.get_messages(entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id))
                         #msg_o.message,file=msg_o.media
                         await handler.reply(trigger.reply, trigger.f_mesg_id)
                     #elif trigger.reply:
